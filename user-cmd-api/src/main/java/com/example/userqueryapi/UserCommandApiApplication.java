b567c566287a3b9ec1d01cb614b170f3c3ededed
mbimbij <joseph.mbimbi@gmail.com>
2021-08-19 16:21:23 +0200

    Initial Commit

---
+++ user-cmd-api/src/main/java/com/example/userqueryapi/UserCommandApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.userqueryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandApiApplication.class, args);
+	}
+
+}
---
+++ user-cmd-api/src/test/java/com/example/userqueryapi/UserCommandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCommandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
